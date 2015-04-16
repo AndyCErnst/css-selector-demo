76357913d2db013c4d347df0fc46c454fa2b2bbf
Andy Ernst <andycernst@gmail.com>
2015-04-16 15:28:01 -0700

    start refactor to allow two style sheets

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var $stylesheet = $('#style');
-var stylesheetRules = ' {\n\tbackground-color: green;\n}';
-var $stylesheetListing = $('#stylesheet-listing');
-var classCounter = 1;
+var $stylesheets = 
+	[$('#style1'), $('#style2')];
+var ssRules = 
+	[' {\n\tbackground-color: red;\n}',
+	' {\n\tbackground-color: green;\n}'];
+var $ssLists = 
+	[ $('#1st-stylesheet-listing'), 
+	$('#2nd-stylesheet-listing')];
+var classCounter = [1,1];
 var $box = $('#box');
 
-$('#add-class').on('click', function(){
-	var newClass = '.c'+ classCounter++;
+
+(function init(){
+	$ssLists[0].text(ssRules[0]);
+	$ssLists[1].text(ssRules[1]);
+})()
+
+var addOneClass = function(num) {
+	var newClass = 'c'+ classCounter[0]++;
 	$box.addClass(newClass);
-	stylesheetRules = newClass + stylesheetRules;
-	$stylesheet.text(stylesheetRules);
-	$stylesheetListing.text(stylesheetRules);
+	newClass = '.' + newClass;
+	ssRules[1] = newClass + ssRules[1];
+	$stylesheets[1].text(ssRules[1]);
+	$ssLists[1].text(ssRules[1]);
+}
+
+$('#add-class').on('click', addOneClass);
+
+$('#add-10-classes').on('click', function(){
+	var i = 10;
+	while(i--) {
+		addOneClass();
+	}
 });
+
