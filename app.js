c49b71a121aa265216a976149d2d630f769262be
Andy Ernst <andycernst@gmail.com>
2015-04-16 17:52:56 -0700

    add support for up to 8 sheets

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
-var $stylesheets = 
-	[$('#style1'), $('#style2')];
-var ssRules = 
-	[' {\n\tbackground-color: red;\n}',
-	' {\n\tbackground-color: green;\n}'];
-var $ssLists = 
-	[ $('#1st-stylesheet-listing'), 
-	$('#2nd-stylesheet-listing')];
-var classCounter = [0,0];
+var stylesheets = [];
 var $box = $('#box');
+var sheetDisplayTemplate = _.template($('#sheet-section-template').text());
+var currentSheetNum = 0;
+var colors = ['gray', 'pink', 'green', 'blue', 'purple', 'orange', 'yellow', 'red'];
 
+var addStylesheet = function(){
+	currentSheetNum++;
+	var color = colors.pop();
+	var newRule = ' {\n\tbackground-color: ' + color + ';\n}';
+	var sheetDisplay = sheetDisplayTemplate({number: currentSheetNum, rule: newRule, color: color});
+	var newSheet = {
+		sheet: $('<style>'+newRule+'</style>').appendTo('head'),
+		rule: newRule,
+		counter: 0,
+		display: $(sheetDisplay).appendTo($('#stylesheet-section')).find('.sheet-display')
+	};
+	stylesheets.push(newSheet);
+};
 
-(function init(){
-	$ssLists[0].text(ssRules[0]);
-	$ssLists[1].text(ssRules[1]);
-})()
+$('#add-stylesheet').on('click', addStylesheet);
 
 var addOneClass = function(num) {
 	console.log('num is ' + num);
-	classCounter[num]++;
-	$box.addClass('c'+classCounter[num]);
-	ssRules[num] = '.c'+classCounter[num] + ssRules[num];
-	$stylesheets[num].text(ssRules[num]);
-	$ssLists[num].text(ssRules[num]);
-}
+	var stylesheet = stylesheets[num];
+	console.log('sheet is ');
+	console.dir(stylesheet);
+	stylesheet.counter++;
+	$box.addClass('c'+stylesheet.counter);
+	stylesheet.rule = '.c'+stylesheet.counter + stylesheet.rule;
+	stylesheet.sheet.text(stylesheet.rule);
+	stylesheet.display.text(stylesheet.rule);
+};
 
-$('.add-class').on('click', function(){
+$('#stylesheet-section').on('click', '.add-class', function(){
+	console.log(this);
 	addOneClass($(this).closest('.section').data('id')-1);
 });
 
-$('.add-10-classes').on('click', function(){
-	var id = $(this).closest('.section').data('id');
+$('#stylesheet-section').on('click', '.add-10-classes', function(){
+	var id = $(this).closest('.section').data('id')-1;
 	var i = 10;
 	while(i--) {
-		addOneClass(id-1);
+		addOneClass(id);
 	}
 });
 
+(function init(){
+	addStylesheet();
+})();
